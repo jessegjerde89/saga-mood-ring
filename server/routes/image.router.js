b617b883d3e0f92114ec315ab25df64194090368
Jesse Gjerde <jessegjerde89@gmail.com>
2019-05-19 09:16:43 -0500

    moved post route and updated as well

--- server/routes/image.router.js
+++ server/routes/image.router.js
@@ -3,7 +3,7 @@ const pool = require('../modules/pool');
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
+router.get('/images', (req, res) => {
     const queryText = 'SELECT * FROM images'
     pool.query(queryText)
     .then(result => { 
@@ -12,6 +12,20 @@ router.get('/', (req, res) => {
         console.log('Error in get call', error);
         res.sendStatus(500); 
     })
+})
+
+
+router.post(`/images/addtags`, (req,res) => {
+    const queryText = 
+    (`INSERT INTO "images_tags" ("images_id", "tag_id")
+    VALUES ($1, $2)`)
+    pool.query(queryText[req.query.image_id, req.query.tag_id])
+.then( response => {
+    console.log('in put tag', response )
+    res.sendStatus(201); 
+}).catch( error => {
+    res.sendStatus(500); 
+})
 })
 
 
